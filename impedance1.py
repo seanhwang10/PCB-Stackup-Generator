064f413efaa32db461e336438e4e840d31d43bac
Sean Hwang <hwang103@purdue.edu>
2023-07-19 11:32:26 -0700

    Update impedance1.py

--- impedance1.py
+++ impedance1.py
@@ -1,26 +1,45 @@
+# Impedance 1 - User input and tabs for all types 
+
 import tkinter as tk
 from tkinter import ttk
-import math
 
 def calculate_impedance():
-    # Get user inputs from the entry widgets
-    w = float(w_entry.get())
-    s = float(s_entry.get())
-    t = float(t_entry.get())
-    h_1 = float(h1_entry.get())
-    impedance_type = impedance_notebook.index(impedance_notebook.select())
+    # Get user inputs from the entry widgets based on the selected tab
+    selected_tab = impedance_notebook.index(impedance_notebook.select())
 
-    if impedance_type == 0:  # Single Ended
-        Er_1 = float(Er1_entry.get())
-        impedance = w / (2 * 3.141592653589793 * s * t) * (1 + (1 + 2 * h_1 / w) ** 0.5)
-        impedance *= (1 - 0.48 * math.exp(-0.96 * (Er_1 - 1)) + 0.54 * ((Er_1 - 1) ** 2))
-    else:  # Differential
-        h_2 = float(h2_entry.get())
-        Er_1 = float(Er1_entry.get())
-        Er_2 = float(Er2_entry.get())
-        impedance = w / (2 * 3.141592653589793 * s * t) * ((1 + (2 * h_1 / w) ** 0.5) + (1 + (2 * h_2 / w) ** 0.5))
-        impedance *= (1 - 0.48 * math.exp(-0.96 * (Er_1 - 1)) + 0.54 * ((Er_1 - 1) ** 2))
-        impedance *= (1 - 0.48 * math.exp(-0.96 * (Er_2 - 1)) + 0.54 * ((Er_2 - 1) ** 2))
+    if selected_tab == 0:  # Microstrip - SE
+        w = float(microstrip_se_w_entry.get())
+        t = float(microstrip_se_t_entry.get())
+        h = float(microstrip_se_h_entry.get())
+        Er = float(microstrip_se_Er_entry.get())
+        impedance = w / (2 * 3.141592653589793 * t) * (1 + (1 + 12 * h / w) ** 0.5)
+        impedance *= (1 + 0.04 * (Er - 1) / (Er + 1))
+    elif selected_tab == 1:  # Microstrip - Diff
+        w = float(microstrip_diff_w_entry.get())
+        s = float(microstrip_diff_s_entry.get())
+        t = float(microstrip_diff_t_entry.get())
+        h = float(microstrip_diff_h_entry.get())
+        Er = float(microstrip_diff_Er_entry.get())
+        impedance = w / (2 * 3.141592653589793 * s * t) * (1 + (1 + 12 * h / w) ** 0.5)
+        impedance *= (1 + 0.04 * (Er - 1) / (Er + 1))
+    elif selected_tab == 2:  # Stripline - SE
+        w = float(stripline_se_w_entry.get())
+        t = float(stripline_se_t_entry.get())
+        h1 = float(stripline_se_h1_entry.get())
+        Er1 = float(stripline_se_Er1_entry.get())
+        impedance = w / (2 * 3.141592653589793 * t) * (1 + (1 + 2 * h1 / w) ** 0.5)
+        impedance *= (1 - 0.48 * math.exp(-0.96 * (Er1 - 1)) + 0.54 * ((Er1 - 1) ** 2))
+    else:  # Stripline - Diff
+        w = float(stripline_diff_w_entry.get())
+        s = float(stripline_diff_s_entry.get())
+        t = float(stripline_diff_t_entry.get())
+        h1 = float(stripline_diff_h1_entry.get())
+        h2 = float(stripline_diff_h2_entry.get())
+        Er1 = float(stripline_diff_Er1_entry.get())
+        Er2 = float(stripline_diff_Er2_entry.get())
+        impedance = w / (2 * 3.141592653589793 * s * t) * ((1 + (2 * h1 / w) ** 0.5) + (1 + (2 * h2 / w) ** 0.5))
+        impedance *= (1 - 0.48 * math.exp(-0.96 * (Er1 - 1)) + 0.54 * ((Er1 - 1) ** 2))
+        impedance *= (1 - 0.48 * math.exp(-0.96 * (Er2 - 1)) + 0.54 * ((Er2 - 1) ** 2))
 
     # Update the output label with the calculated impedance
     output_label.config(text=f"Impedance: {impedance:.2f} Ohms")
@@ -34,50 +53,125 @@ root.geometry("400x300")
 impedance_notebook = ttk.Notebook(root)
 impedance_notebook.pack(fill=tk.BOTH, expand=True)
 
-# Create the Single Ended tab
-single_ended_tab = ttk.Frame(impedance_notebook)
-impedance_notebook.add(single_ended_tab, text="Single Ended")
+# Create the Microstrip - SE tab
+microstrip_se_tab = ttk.Frame(impedance_notebook)
+impedance_notebook.add(microstrip_se_tab, text="Microstrip - SE")
 
-# Create entry widgets for Single Ended inputs
-w_label = tk.Label(single_ended_tab, text="Enter w:")
-w_label.pack()
-w_entry = tk.Entry(single_ended_tab)
-w_entry.pack()
+# Create entry widgets for Microstrip - SE inputs
+microstrip_se_w_label = tk.Label(microstrip_se_tab, text="Enter w:")
+microstrip_se_w_label.pack()
+microstrip_se_w_entry = tk.Entry(microstrip_se_tab)
+microstrip_se_w_entry.pack()
 
-s_label = tk.Label(single_ended_tab, text="Enter s:")
-s_label.pack()
-s_entry = tk.Entry(single_ended_tab)
-s_entry.pack()
+microstrip_se_t_label = tk.Label(microstrip_se_tab, text="Enter t:")
+microstrip_se_t_label.pack()
+microstrip_se_t_entry = tk.Entry(microstrip_se_tab)
+microstrip_se_t_entry.pack()
 
-t_label = tk.Label(single_ended_tab, text="Enter t:")
-t_label.pack()
-t_entry = tk.Entry(single_ended_tab)
-t_entry.pack()
+microstrip_se_h_label = tk.Label(microstrip_se_tab, text="Enter h:")
+microstrip_se_h_label.pack()
+microstrip_se_h_entry = tk.Entry(microstrip_se_tab)
+microstrip_se_h_entry.pack()
 
-h1_label = tk.Label(single_ended_tab, text="Enter h_1:")
-h1_label.pack()
-h1_entry = tk.Entry(single_ended_tab)
-h1_entry.pack()
+microstrip_se_Er_label = tk.Label(microstrip_se_tab, text="Enter Er:")
+microstrip_se_Er_label.pack()
+microstrip_se_Er_entry = tk.Entry(microstrip_se_tab)
+microstrip_se_Er_entry.pack()
 
-Er1_label = tk.Label(single_ended_tab, text="Enter Er_1:")
-Er1_label.pack()
-Er1_entry = tk.Entry(single_ended_tab)
-Er1_entry.pack()
+# Create the Microstrip - Diff tab
+microstrip_diff_tab = ttk.Frame(impedance_notebook)
+impedance_notebook.add(microstrip_diff_tab, text="Microstrip - Diff")
 
-# Create the Differential tab
-differential_tab = ttk.Frame(impedance_notebook)
-impedance_notebook.add(differential_tab, text="Differential")
+# Create entry widgets for Microstrip - Diff inputs
+microstrip_diff_w_label = tk.Label(microstrip_diff_tab, text="Enter w:")
+microstrip_diff_w_label.pack()
+microstrip_diff_w_entry = tk.Entry(microstrip_diff_tab)
+microstrip_diff_w_entry.pack()
 
-# Create entry widgets for Differential inputs
-h2_label = tk.Label(differential_tab, text="Enter h_2:")
-h2_label.pack()
-h2_entry = tk.Entry(differential_tab)
-h2_entry.pack()
+microstrip_diff_s_label = tk.Label(microstrip_diff_tab, text="Enter s:")
+microstrip_diff_s_label.pack()
+microstrip_diff_s_entry = tk.Entry(microstrip_diff_tab)
+microstrip_diff_s_entry.pack()
 
-Er2_label = tk.Label(differential_tab, text="Enter Er_2:")
-Er2_label.pack()
-Er2_entry = tk.Entry(differential_tab)
-Er2_entry.pack()
+microstrip_diff_t_label = tk.Label(microstrip_diff_tab, text="Enter t:")
+microstrip_diff_t_label.pack()
+microstrip_diff_t_entry = tk.Entry(microstrip_diff_tab)
+microstrip_diff_t_entry.pack()
+
+microstrip_diff_h_label = tk.Label(microstrip_diff_tab, text="Enter h:")
+microstrip_diff_h_label.pack()
+microstrip_diff_h_entry = tk.Entry(microstrip_diff_tab)
+microstrip_diff_h_entry.pack()
+
+microstrip_diff_Er_label = tk.Label(microstrip_diff_tab, text="Enter Er:")
+microstrip_diff_Er_label.pack()
+microstrip_diff_Er_entry = tk.Entry(microstrip_diff_tab)
+microstrip_diff_Er_entry.pack()
+
+# Create the Stripline - SE tab
+stripline_se_tab = ttk.Frame(impedance_notebook)
+impedance_notebook.add(stripline_se_tab, text="Stripline - SE")
+
+# Create entry widgets for Stripline - SE inputs
+stripline_se_w_label = tk.Label(stripline_se_tab, text="Enter w:")
+stripline_se_w_label.pack()
+stripline_se_w_entry = tk.Entry(stripline_se_tab)
+stripline_se_w_entry.pack()
+
+stripline_se_t_label = tk.Label(stripline_se_tab, text="Enter t:")
+stripline_se_t_label.pack()
+stripline_se_t_entry = tk.Entry(stripline_se_tab)
+stripline_se_t_entry.pack()
+
+stripline_se_h1_label = tk.Label(stripline_se_tab, text="Enter h_1:")
+stripline_se_h1_label.pack()
+stripline_se_h1_entry = tk.Entry(stripline_se_tab)
+stripline_se_h1_entry.pack()
+
+stripline_se_Er1_label = tk.Label(stripline_se_tab, text="Enter Er_1:")
+stripline_se_Er1_label.pack()
+stripline_se_Er1_entry = tk.Entry(stripline_se_tab)
+stripline_se_Er1_entry.pack()
+
+# Create the Stripline - Diff tab
+stripline_diff_tab = ttk.Frame(impedance_notebook)
+impedance_notebook.add(stripline_diff_tab, text="Stripline - Diff")
+
+# Create entry widgets for Stripline - Diff inputs
+stripline_diff_w_label = tk.Label(stripline_diff_tab, text="Enter w:")
+stripline_diff_w_label.pack()
+stripline_diff_w_entry = tk.Entry(stripline_diff_tab)
+stripline_diff_w_entry.pack()
+
+stripline_diff_s_label = tk.Label(stripline_diff_tab, text="Enter s:")
+stripline_diff_s_label.pack()
+stripline_diff_s_entry = tk.Entry(stripline_diff_tab)
+stripline_diff_s_entry.pack()
+
+stripline_diff_t_label = tk.Label(stripline_diff_tab, text="Enter t:")
+stripline_diff_t_label.pack()
+stripline_diff_t_entry = tk.Entry(stripline_diff_tab)
+stripline_diff_t_entry.pack()
+
+stripline_diff_h1_label = tk.Label(stripline_diff_tab, text="Enter h_1:")
+stripline_diff_h1_label.pack()
+stripline_diff_h1_entry = tk.Entry(stripline_diff_tab)
+stripline_diff_h1_entry.pack()
+
+stripline_diff_h2_label = tk.Label(stripline_diff_tab, text="Enter h_2:")
+stripline_diff_h2_label.pack()
+stripline_diff_h2_entry = tk.Entry(stripline_diff_tab)
+stripline_diff_h2_entry.pack()
+
+stripline_diff_Er1_label = tk.Label(stripline_diff_tab, text="Enter Er_1:")
+stripline_diff_Er1_label.pack()
+stripline_diff_Er1_entry = tk.Entry(stripline_diff_tab)
+stripline_diff_Er1_entry.pack()
+
+stripline_diff_Er2_label = tk.Label(stripline_diff_tab, text="Enter Er_2:")
+stripline_diff_Er2_label.pack()
+stripline_diff_Er2_entry = tk.Entry(stripline_diff_tab)
+stripline_diff_Er2_entry.pack()
 
 # Create the "Calculate Impedance" button
 calculate_button = tk.Button(root, text="Calculate Impedance", command=calculate_impedance)
